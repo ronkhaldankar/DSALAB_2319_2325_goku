34a65cf12c432e499e386cb0f6e1dd2f4673dc2f
Smruthul M S <97047561+Smruthul12@users.noreply.github.com>
2023-08-03 12:51:00 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,46 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "stack.h"
 int main()
 {
-    printf("\nhello");
-    int n;
+	printf("\nhello");
+	int n;
+	Stack mystack;
+	init(&mystack);
 	do
 	{
-		printf("\nMenu:\n0: Exit \n1: Insertion \n2: Selection \n3: Bubble");
+		printf("\nMenu:\n0: Exit \n1: Quicksort sort \n2: Merge sort \n3: Stack");
 		printf("\nEnter value of n:");
-    	scanf("%d",&n);
-    	
-		switch(n){
-	    	case 1:
-	    		printf("\nInsertion sort\n");
-	    		break;
-	    	case 2:
-	    		printf("\nSelection sort\n");
-	    		break;
-	    	case 3:
-	    		printf("\nBubble sort\n");
-	    		break;
-	    	case 0:
-	    	default:
-	    		printf("\nNo sorting for you\n");
-	    		break;
+		scanf("%d", &n);
+
+		switch (n)
+		{
+		case 1:
+			printf("\nQuicksort sort\n");
+			break;
+		case 2:
+			printf("\nMerge sort\n");
+			break;
+		case 3:
+			printf("\nStack\n");
+			//push
+			push(&mystack, 10);
+			push(&mystack, 29);
+			push(&mystack, 22);
+
+			// pop
+			while (!is_empty(&mystack))
+			{
+				int item = pop(&mystack);
+				printf("%d\n", item);
+			}
+			break;
+		case 0:
+		default:
+			printf("\nNo sorting for you\n");
+			break;
 		}
-		
-	}while(n!=0);
-	
-    return 0;
+
+	} while (n != 0);
+
+	return 0;
 }
